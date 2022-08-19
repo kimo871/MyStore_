85faa2b05c8b17e9760a9bda05380a780e9b9bbb
kimo871 <mk5391130@gmail.com>
2022-08-19 04:07:05 +0200

    o

--- src/Login.js
+++ src/Login.js
@@ -9,7 +9,7 @@ function Login({search}){
    let [email,upEmail] =useState(" ");
    let [users,edit] =useState([]);
    let [pass,upPass] = useState(" ");
-  // let[err,find] =useState(false);
+   let[err,find] =useState(false);
    let navigate =useNavigate();
 
    useEffect(()=>{
@@ -26,22 +26,26 @@ function Login({search}){
 		console.log("Email is " + email);
 		console.log("Password is " + pass);
 		event.preventDefault();
-		let result = users.filter((user)=> (user.email ===email && user.password ===pass));
+		let result = users.filter((user)=> (user.email ==email && user.password ===pass));
     if(result.length >0){
     search(result[0]);
+    find(false);
     navigate("/")
     }
+    else{find(true)}
     return result;
  
 	}
-return (<>
+return (
+  <div>
 <Navbar logged={false} form={true}/>
 {err && (
-<div className='err-cont'>
-<h4>Email or Password Not Correct</h4>
+	<center>
+<div className="err-cont">
+<h4>Email or Password not correct !</h4>
 </div>
-  )}
-<br>
+</center>
+)}
 <div className="Container">
 <div className="form">
 <h1><center>Login To Your Shop Account.</center></h1>
@@ -64,7 +68,7 @@ return (<>
   </form>
 </div>
 </div>
-</>
+</div>
 )	
 
 }
